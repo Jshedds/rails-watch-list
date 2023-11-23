cc7d125408aa4a2fdefa3850828f6654a13a5b91
Jamie Sheddan <jsheddan@outlook.com>
2023-11-23 16:57:59 +0000

    uncompleted watch list

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,22 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'json'
+require 'open-uri'
+
+Movie.destroy_all
+
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+movies_string = URI.open(url).read
+movies = JSON.parse(movies_string)
+
+movies['results'].each do |movie|
+  movie = Movie.create!(
+    title: movie['title'],
+    overview: movie['overview'],
+    poster_url: movie['poster_path'],
+    rating: movie['vote_average']
+  )
+  puts "created #{movie.title}"
+end
