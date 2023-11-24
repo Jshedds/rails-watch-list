8a62a7dfa0f4cd77bd872600d02ba290f4ade38b
Jamie Sheddan <jsheddan@outlook.com>
2023-11-24 14:26:32 +0000

    delete function added

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -8,8 +8,18 @@ class BookmarksController < ApplicationController
     @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
-    @bookmark.save
-    redirect_to list_path(@list)
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    # @list = List.find(params[:list_id])
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy!
+    redirect_to list_path(@bookmark.list), status: :see_other
   end
 
   private
